72b370e32656e9f67ded2cd4e8bba65241b0bb22
MoffKalast <vid.rijavec@gmail.com>
2023-06-27 22:26:37 +0200

    fixed sanctuary and new port

--- js/grid_routes.js
+++ js/grid_routes.js
@@ -9,8 +9,8 @@ let trade_routes = [
 	[[[-5.1, 32.75], [0.63, 40.3]],"downwind"], //oasis -> fort aestrin
 	[[[-5.1, 32.75], [3.5802, 35.363]],"downwind"], //oasis -> happy bay
 	[[[0.63, 40.3], [3.5802, 35.363]],"beamreach"],  //fort aestrin -> happy bay
-	[[[3.5802, 35.363], [4.79, 31.4]],"beamreach"], //happy bay -> dragon cliffs
-	[[[4.75, 31.3], [-2, 31.14]],"downwind"], //dragon cliffs -> midpoint
+	[[[3.5802, 35.363], [4.77, 31.33]],"beamreach"], //happy bay -> dragon cliffs
+	[[[4.77, 31.33], [-2, 31.14]],"downwind"], //dragon cliffs -> midpoint
 	[[[-2, 31.14], [-4.8682, 31.14]],"closehauled"], //midpoint -> gold rock
 ];
 
@@ -35,11 +35,12 @@ var secretRouteJson = {
 let secret_routes = [
 	[[[0.63, 40.3], [22.42764, 38.4074]],"downwind"], // fort aestrin -> chronos
 	[[[3.5802, 35.363], [22.42764, 38.4074]],"downwind"], // happy bay -> chronos
-	[[[4.79, 31.4], [5.9867, 33.0134]],"closehauled"], // dragon cliffs -> teapoint
+	[[[4.77, 31.33], [5.9867, 33.0134]],"closehauled"], // dragon cliffs -> teapoint
 	[[[5.9867, 33.0134], [22.42764, 38.4074]],"downwind"], // teapoint -> chronos
 	[[[22.42764, 38.4074], [22.381017, 31.510734]],"closehauled"], // chronos -> dogleg
-	[[[22.381017, 31.510734], [4.75, 31.3]],"downwind"], // dogleg  -> dragon cliffs
+	[[[22.381017, 31.510734], [4.77, 31.33]],"downwind"], // dogleg  -> dragon cliffs
 ];
+
 
 
 for (const route of secret_routes){
