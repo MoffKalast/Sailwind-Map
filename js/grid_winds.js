fe09284cc2f155e1f51ce917e783bc75f66cd84d
MoffKalast <vid.rijavec@gmail.com>
2025-03-28 22:29:50 +0100

    updated trade winds

--- js/grid_winds.js
+++ js/grid_winds.js
@@ -5,14 +5,30 @@ var windJson = {
 
 //From https://sailwind.fandom.com/wiki/Trade_winds
 
+//last known in-game code
+/* if (z < 30f){
+    return Vector3.zero;
+}
+if (z > 33){
+    return new Vector3(1f, 0f, 0.5f).normalized;
+}
+if (x < -2f){
+    return new Vector3(0.75f, 0f, 0.75f).normalized;
+}
+if (z < 32){
+    return new Vector3(-1f, 0f, -0.5f).normalized;
+}
+return Vector3.zero; */
+
+
 let skip = false;
 
 for (let long = minLong+0.5; long <= maxLong; long++) {
 	for (let lat = minLat+0.5; lat <= maxLat; lat++) {
 		
 		if(lat > 33){
-			let deltax = Math.sin(1.106539)*0.15;
-			let deltay = Math.cos(1.106539)*0.15;
+			let deltax = 0.134164;//Math.sin(1.106539)*0.15;
+			let deltay = 0.0670820;//Math.cos(1.106539)*0.15;
 			windJson.features.push({
 				"type": "Feature",
 				"geometry": {
@@ -24,9 +40,9 @@ for (let long = minLong+0.5; long <= maxLong; long++) {
 				}
 			});
 		}
-		else if(long < -2){
-			let deltax = Math.sin(0.78539)*0.15;
-			let deltay = Math.cos(0.78539)*0.15;
+		else if(long < -2 && lat > 30){
+			let deltax = 0.106066;//Math.sin(0.78539)*0.15;
+			let deltay = 0.106066;//Math.cos(0.78539)*0.15;
 			windJson.features.push({
 				"type": "Feature",
 				"geometry": {
@@ -38,9 +54,9 @@ for (let long = minLong+0.5; long <= maxLong; long++) {
 				}
 			});
 		}
-		else if(lat < 32){
-			let deltax = Math.sin(4.2481314)*0.15;
-			let deltay = Math.cos(4.2481314)*0.15;
+		else if(lat < 32 && lat > 30){
+			let deltax = -0.1341640;//Math.sin(4.2481314)*0.15;
+			let deltay =  -0.0670828;//Math.cos(4.2481314)*0.15;
 			windJson.features.push({
 				"type": "Feature",
 				"geometry": {
