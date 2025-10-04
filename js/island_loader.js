7d737bd0623701c063e57c4ceec8ea6b36f5f78e
SnakeSel <snake.sel@gmail.com>
2025-10-04 10:17:07 +0300

    feat: old_ankh_town to island.loader.js

--- js/island_loader.js
+++ js/island_loader.js
@@ -53,6 +53,9 @@ const island_files = [
     "alankh/rocks/gold_rock_city_rock_0",
     "alankh/rocks/gold_rock_city_rock_1",
     "alankh/rocks/oasis_rock",
+    "alankh/old_ankh_town_0.json",
+    "alankh/old_ankh_town_2.json",
+    "alankh/cities/old_ankh_town_city.json",
 
     "bay/happy_bay_0",
     "bay/happy_bay_1",
