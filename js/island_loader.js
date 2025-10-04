69558280a9e206f3dcd8ed3a23993c6dc877a098
MoffKalast <vid.rijavec@gmail.com>
2025-10-04 09:58:57 +0200

    old ankh town and island border fix, converted to multipolygon

--- js/island_loader.js
+++ js/island_loader.js
@@ -53,8 +53,7 @@ const island_files = [
     "alankh/rocks/gold_rock_city_rock_0",
     "alankh/rocks/gold_rock_city_rock_1",
     "alankh/rocks/oasis_rock",
-    "alankh/old_ankh_town_0",
-    "alankh/old_ankh_town_1",
+    "alankh/old_ankh_town",
     "alankh/cities/old_ankh_town_city",
 
     "bay/happy_bay_0",
